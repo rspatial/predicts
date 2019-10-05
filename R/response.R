8ef5811595541bddaf37c7dbf678a1b7bf166457
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2019-10-04 20:54:05 -0700

    ok

--- R/response.R
+++ R/response.R
@@ -18,24 +18,48 @@ varImportance <- function(mod, dat, vars, n=10) {
 }
 
 
-partialResponse <- function(mod, dat, var, nsteps=25) {
-	if (is.factor(data[[var]])) {
-		steps <- levels(data[[var]]])
+partialResponse <- function(model, data, variable, rng=NULL, nsteps=25) {
+	if (is.factor(data[[variable]])) {
+		steps <- levels(data[[variable]])
 	} else {
 		if (is.null(rng)) { 
-			rng <- range(dat[[var]]) 
+			rng <- range(data[[variable]]) 
 		}
 		increment <- (rng[2] - rng[1])/(nsteps-2)
 		steps <- seq(rng[1]-increment, rng[2]+increment, increment)
 	}
 	res <- rep(NA, length(steps))
 	for (i in 1:length(steps)) {
-		dat[[var]] <- steps[i]
-		p <- predict(mod, dat)
+		data[[variable]] <- steps[i]
+		p <- predict(model, data)
 		res[i] <- mean(p)
 	}
-	data.frame(var=steps, p=res)
+	data.frame(variable=steps, p=res)
 }
 
+
+partialResponse2 <- function(model, data, var1, var2, var2levels, rng=NULL, nsteps=25) {
+	if (is.factor(data[[var]])) {
+		steps <- levels(data[[var]])
+	} else {
+		if (is.null(rng)) { 
+			rng <- range(data[[var]]) 
+		}
+		increment <- (rng[2] - rng[1])/(nsteps-2)
+		steps <- seq(rng[1]-increment, rng[2]+increment, increment)
+	}
+	res <- rep(NA, length(steps))
+	out <- data.frame(var=steps)	
+	for (v in var2levels) {
+		data[[var2]] <- v
+		for (i in 1:length(steps)) {
+			data[[var]] <- steps[i]
+			p <- predict(model, data)
+			res[i] <- mean(p)
+		}
+		out[[paste(var2, v, sep="_")]] <- res
+	}
+	out
+}
 
 
